a0628d63271c2aaaf2d553765e9f5ed67081fb59
Luis Fernando <luissierradelgado6@gmail.com>
2025-07-27 23:11:09 -0600

    Se cambio la sintaxis tomando de base las de la api en clase. de las  funciones y rutas de las tareas

--- app/controllers/tareasController.js
+++ app/controllers/tareasController.js
@@ -11,51 +11,93 @@ function buscarTodo(req, res) {
     .catch(e => {return res.status(404).send({mensaje: `Eror al consultar la información ${e}`})})
 }
 
-async function crearTarea(req, res) {
-    try {
-        const nuevaTarea = new tareasModel({
-            ...req.body,
-            alumnoId: req.usuario.id // <-- obtenido del token
-        });
-        await nuevaTarea.save();
-        res.status(201).json(nuevaTarea);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+function agregarTarea(req, res) {
+
+    const nuevaTarea = {
+        ...req.body,
+        alumnoId: req.usuario.id // <-- Agrega el ID del alumno desde el token
+    };
+    
+    new tareasModel(nuevaTarea).save()
+    .then(info => {
+        return res.status(200).send({
+            mensaje: "La información se guardo de forma correcta",
+            info
+        })
+    })
+    .catch(e => {return res.status(404).send({
+        mensaje:`error al guardar ${e}`
+    })})
 }
 
-async function actualizarTarea(req, res) {
-    const { id } = req.params;
-    try {
-        const tarea = await tareasModel.findOneAndUpdate(
-            { _id: id, alumnoId: req.usuario.id }, // seguridad
-            req.body,
-            { new: true }
-        );
-        if (!tarea) return res.status(404).send({ mensaje: 'Tarea no encontrada' });
-        res.json(tarea);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+function buscarTarea(req, res, next) {
+    if (!req.body) req.body = {}
+
+    var consulta = {}
+    consulta[req.params.key] = req.params.value
+    
+    tareasModel.find(consulta)
+    .then(tareas => {
+        if (!tareas.length) return next();
+        req.body.tareas = tareas
+        return next()
+    })
+    .catch(e => {
+        req.body.e = e
+        return next()
+    })
 }
 
+function eliminarTarea(req, res) {
+    var tareas = {}
+    tareas = req.body.tareas
 
-async function eliminarTarea(req, res) {
-    const { id } = req.params;
-    try {
-        const tarea = await tareasModel.findOneAndDelete({ _id: id, alumnoId: req.usuario.id });
-        if (!tarea) return res.status(404).send({ mensaje: 'Tarea no encontrada' });
-        res.json({ mensaje: 'Tarea eliminada' });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+    tareasModel.deleteOne(tareas[0])
+    .then(info =>{
+        return res.status(200).send({mensaje: "La información se elimino de forma correcta", info})
+    })
+
+    .catch(e => {
+        return res.status(404).send({mensaje: "error al eliminar información", e})
+    })
+
 }
 
+function actualizarTarea(req, res) {
+    tareas = req.body.tareas
+    
+    if (!tareas || !tareas.length) {
+        return res.status(200).send({
+            mensaje: "No se encontró la tarea modificada"
+        })
+    }
+
+    tareasModel.updateOne(tareas[0], req.body)
+    .then(info => {
+        return res.status(200).send({
+            mensaje: "La información se actualizó correctamente",
+            info
+        })
+    })
+    .catch(e => {
+        return res.status(404).send({
+            mensaje: "Error al actualizar la información", e
+        })
+    })
+}
+
+function mostrarTarea(req, res) {
+    if(req.body.e){return res.status(404).send({mensaje: `error al buscar la información`})}
+    if(!req.body.tareas){return res.status(204).send({mensaje: `No hay nada que mostrar`})}
+    let tareas = req.body.tareas
+    return res.status(200).send({tareas})
+}
 
 module.exports = {
-    crearTarea,
     actualizarTarea,
     eliminarTarea,
-
-    buscarTodo
+    buscarTodo,
+    agregarTarea,
+    buscarTarea,
+    mostrarTarea
 };
--- app/routes/tareasRoutes.js
+++ app/routes/tareasRoutes.js
@@ -4,11 +4,10 @@ const tareasController = require('../controllers/tareasController');
 const auth = require('../middlewares/auth');
 
 router.use(auth.verificarToken);
-
 router.get('/', tareasController.buscarTodo);
-router.post('/', tareasController.crearTarea);
-router.put('/:id', tareasController.actualizarTarea);
-router.delete('/:id', tareasController.eliminarTarea);
-
+router.post('/', tareasController.agregarTarea);
+router.get('/:key/:value', tareasController.buscarTarea, tareasController.mostrarTarea);
+router.delete('/:key/:value', tareasController.buscarTarea, tareasController.eliminarTarea);
+router.put('/:key/:value', tareasController.buscarTarea, tareasController.actualizarTarea);
 
 module.exports = router;
